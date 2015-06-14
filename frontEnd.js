585eb52e6a8b9012172cf7ce4eda55211ada2ceb
Anita <a.nita@hotmail.co.uk>
2015-06-14 20:29:02 +0100

    saving new bubbles to data.json as objects in an array

--- frontEnd.js
+++ frontEnd.js
@@ -6,7 +6,20 @@
         return "<div class='bubble'>" + "<p>" + data.body + "</p>" + "</div>";
     }
 
-    $("#bubbleForm").submit(function( event ) {
+    window.onload = function() {
+        $.get("/all", function(data) {
+            var bubbles = JSON.parse(data);
+            //var bubbles = obj.bubbles;
+            console.log(bubbles);
+            var accessDOM = "";
+            for (var i = 0; i < bubbles.length; i++) {
+                accessDOM += addBubble(bubbles[i]);
+            }
+            $("#bubbles").prepend(accessDOM);
+        });
+    };
+
+    $("#bubbleForm").submit(function(event) {
         //alert( "Handler for .submit() called." );
         event.preventDefault();
 
--- handlers.js
+++ handlers.js
@@ -5,31 +5,41 @@ var handlers = (function() {
     var app = {};
 
     app["GET /all"] = function(request, response) {
+        //var bubblesLoad = require(__dirname + "/data.json");
+        //var bubbles = fs.readFileSync(__dirname + "/data.json");
+        //console.log(bubblesLoad);
         response.write("GET all");
+        //response.end(JSON.stringify(bubbles));
+        //response.end(JSON.stringify(bubblesLoad));
         response.end();
     };
 
     app["POST /add"] = function(request, response) {
+        //var bubblesData = [];
         var myBubble = "";
-        //var bubbles = require(__dirname + "/data.json"); //loads the array with all bubbles - tweets
+        //var newBubble;
+        var bubbles = require(__dirname + "/data.json"); //loads the array with all bubbles - tweets
 
         request.on("data", function(chunk) {
-            myBubble += chunk; //this is string because it turns bubble input box buffer into text
+            myBubble += chunk + ""; //this is string because it turns bubble input box buffer into text
             console.log("*********" + myBubble);
         });
 
         request.on("end", function() {
-            //var entry = {body: myBubble, timestamp: Date.now()};
-            var entry = JSON.stringify(({body: myBubble})) + "\n";
+            var entry = {body: myBubble, timestamp: Date.now()};
+            bubbles.push(entry);
+            //var entry = JSON.stringify(({body: myBubble})) + "\n";
             console.log(entry);
+            //bubblesData.push(entry);
             //bubbles.push(entry); //adds new bubble to bubbles array
-            fs.appendFile("data.json", entry, function(err) { //rewrites the file with new bubbles
+            fs.writeFile("data.json", JSON.stringify(bubbles), function(err) { //rewrites the file with new bubbles
                if (err) throw err;
                 console.log("The data to append was append to the file data.json")
             });
             response.writeHead(200, {'Content-Type': 'text/html'});
             //response.write("some data testing")
-            response.end(entry); //sends back new bubble fort display
+            //console.log(bubblesData.toString());
+            response.end(JSON.stringify(entry)); //sends back new bubble for display
         });
     };
     app.generic = function(request, response) {
